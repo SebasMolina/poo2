81e44ce90343af272d68ec4ea20acfe11ff31269
MaxyGomez <maximiliano5598@gmail.com>
2019-11-14 14:12:57 -0300

    insomnia con versiones en las rutas

---
+++ NyanDelivery/src/main/java/newpackage/NewClass.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package newpackage;
+import io.javalin.Javalin;
+
+public class NewClass {
+    public static void main(String[] args) {
+        Javalin app = Javalin.create().start(7000);
+        app.get("/", ctx -> ctx.result("Hello World"));
+    }
+}
